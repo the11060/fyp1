06a5fd4e28bd4ae6f8143dd4b910debd8d83e525
Chan Ting Hei William <the11060@yahoo.com.hk>
2017-02-18 03:11:35 +0800

    express

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
+var logger = require('morgan');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
