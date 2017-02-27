aaa03bb81f8d5ad9d8d406c5c8d2d71e8b6214e6
terrycheng221 <terrycheng@terrychengs-MacBook-Air.local>
2017-02-27 14:58:49 +0800

    Modified by Terry

--- web.js
+++ web.js
@@ -17,10 +17,12 @@ app.listen(port, function() {
 
 var express = require('express');
 var logger = require('morgan');
-var app = require('express')();
+//var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+
+var app = express();
 
 app.get('/', function(request, response) {
   response.send('Hello, Python!');
